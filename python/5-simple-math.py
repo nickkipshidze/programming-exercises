6c6bb197f81a84167c9ddfecf9008b5f25ce0d3c
NickKipshidze <kipshidze.nick@gmail.com>
2024-02-06 06:31:52 +0400

    Add simple math exercise

---
+++ python/5-simple-math.py
@@ -0,0 +1,10 @@
+first = int(input("What is the first number? "))
+second = int(input("What is the second number? "))
+
+print(
+    f"{first} + {second} = {first + second}",
+    f"{first} - {second} = {first - second}",
+    f"{first} * {second} = {first * second}",
+    f"{first} / {second} = {first / second}",
+    sep = "\n"
+)
