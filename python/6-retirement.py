7306429598af765a32aadabae6b8b7124cb72e51
NickKipshidze <kipshidze.nick@gmail.com>
2024-02-09 06:31:52 +0400

    Add retirement exercise

---
+++ python/6-retirement.py
@@ -0,0 +1,4 @@
+age = int(input("What is your current age? "))
+retire = int(input("At what age would you like to retire? "))
+print(f"You have {retire - age} years left until you can retire.")
+print(f"It's 2015, so you can retire in {2015 + retire - age}.")
