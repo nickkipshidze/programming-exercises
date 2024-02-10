068d0fd2236d8b6f5ef85d814d5e99f5fd396db9
NickKipshidze <kipshidze.nick@gmail.com>
2024-02-11 02:32:41 +0400

    Add counting number of characters exercise

---
+++ C/2-counting-chars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    char string[64];
+
+    printf("What is the input string? ");
+    scanf("%s", string);
+
+    printf("%s has %d characters.\n", string, strlen(string));
+
+    return 0;
+}
